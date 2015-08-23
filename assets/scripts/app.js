b47e24f778b22246f81d63b151b96bf4fe1861c0
Murray Smith <murray@mismith.info>
2015-08-23 14:38:07 -0600

    initial commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,79 +1,62 @@
-angular.module('XXXXXX', ['ui.router', 'ui.bootstrap', 'duScroll', 'firebaseHelper', 'contentful', 'hc.marked'])
+angular.module('ut99announcer', [])
 	
 	.run(function(){
 		// remove 300ms click delay on touch devices
 		FastClick.attach(document.body);
-		
-		// fix vh units in ios7 (and others)
-		viewportUnitsBuggyfill.init();
 	})
 	
-	.config(function($locationProvider, $urlRouterProvider, $stateProvider, $firebaseHelperProvider, contentfulProvider){
-		// routing
-		$locationProvider.html5Mode(true);
-		$urlRouterProvider.when('', '/');
-		var pages = [
-			'home'
-		];
-		$stateProvider
-			// pages
-			.state('main', {
-				abstract: true,
-				templateUrl: 'views/main.html',
-			})
-				.state('page', {
-					parent: 'main',
-					url: '/{page:|' + pages.join('|') + '}',
-					templateUrl: function($stateParams){
-						return 'views/page/' + ($stateParams.page || 'home') + '.html';
-					},
-				})
-			// catch-all
-			.state('otherwise', {
-				url: '*path',
-				templateUrl: 'views/page/404.html',
-			});
-		
-		// data
-		$firebaseHelperProvider.namespace('demo');
-		contentfulProvider.setOptions({
-			accessToken: 'XXXXXX',
-			space:       'XXXXXX',
-		});
-	})
-	
-	.controller('AppCtrl', function($rootScope, $state, $document, $location){
-		$rootScope.$state = $state;
-		
-		// smooth scrolling
-		$rootScope.scrollTo = function(id){
-			var el = document.getElementById(id);
-			if(el) $document.scrollToElementAnimated(el, document.getElementById('header').offsetHeight || 0);
-		};
-		$rootScope.scrollTo($location.path().replace(/^\//, ''));
-	})
-	.controller('HomePageCtrl', function($scope, $firebaseHelper, contentful){
-		// angular
-		$scope.angularWorking = 'Yes';
-		
-		// firebase
-		$firebaseHelper.load('example').then(function(example){
-			$scope.firebaseWorking = true;
-		});
-		
-		// contentful
-		contentful.entries('order=sys.createdAt').then(function(response){
-			$scope.entries = response.data.items;
-		});
-	})
-	
-	// smooth scrolling
-	.directive('href', function(){
-		return function($scope, $element, $attrs){
-			if($attrs.href && $attrs.href[0] == '#'){
-				$element.on('click', function(e){
-					$scope.scrollTo(($attrs.href || '').replace(/^#/, ''));
-				});
-			}
+	.controller('AppCtrl', function($scope){
+		$scope.sounds = [
+			[
+				{$id: 'doublekill',   name: 'Double Kill'},
+				{$id: 'triplekill',   name: 'Triple Kill'},
+				{$id: 'multikill',    name: 'Multi Kill'},
+				{$id: 'megakill',     name: 'Mega Kill'},
+				{$id: 'ultrakill',    name: 'Ultra Kill'},
+				{$id: 'monsterkill',  name: 'Monster Kill'},
+			],
+			[
+				{$id: 'headshot',     name: 'Headshot'},
+				{$id: 'killingspree', name: 'Killing Spree'},
+				{$id: 'rampage',      name: 'Rampage'},
+				{$id: 'dominating',   name: 'Dominating'},
+				{$id: 'unstoppable',  name: 'Unstoppable'},
+				{$id: 'godlike',      name: 'Godlike'},
+			],
+			[
+				{$id: 'prepare',      name: 'Prepare for battle'},
+				{$id: 'firstblood',   name: 'First Blood'},
+				{$id: 'takenlead',    name: 'Taken the lead'},
+				{$id: 'lostlead',     name: 'Lost the lead'},
+				{$id: 'lostmatch',    name: 'You have lost the match'},
+				{$id: 'winner',       name: 'Congratulations, you\'re the winner'},
+			],
+			[
+				{$id: 'cd1',          name: '1'},
+				{$id: 'cd2',          name: '2'},
+				{$id: 'cd3',          name: '3'},
+				{$id: 'cd4',          name: '4'},
+				{$id: 'cd5',          name: '5'},
+				{$id: 'cd6',          name: '6'},
+				{$id: 'cd7',          name: '7'},
+				{$id: 'cd8',          name: '8'},
+				{$id: 'cd9',          name: '9'},
+				{$id: 'cd10',         name: '10'},
+			],
+			[
+				{$id: 'assist',       name: 'Assist'},
+				{$id: 'capture',      name: 'Capture'},
+				{$id: 'proceed',      name: 'You may proceed'},
+				{$id: 'failed',       name: 'You have failed to proceed'},
+				{$id: 'lastplace',    name: 'Last place'},
+				{$id: 'cd1min',       name: '1 minute remaining'},
+				{$id: 'cd30sec',      name: '30 seconds left'},
+				{$id: 'cd3min',       name: '3 minutes remaining'},
+				{$id: 'cd5min',       name: '5 minutes remaining'},
+			],
+		];		
+		$scope.play = function (clip) {
+			clip.$audio = clip.$audio || new Audio('assets/audio/' + clip.$id + '.wav');
+			clip.$audio.play();
 		};
 	});
